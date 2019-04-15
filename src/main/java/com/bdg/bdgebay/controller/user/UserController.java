70772cb5c92a025032b2664a990d1de52b27064a
William Arustamyan <williamarustamyan@workfront.com>
2019-04-15 11:44:06 +0400

    Added data transfer object

--- src/main/java/com/bdg/bdgebay/controller/user/UserController.java
+++ src/main/java/com/bdg/bdgebay/controller/user/UserController.java
@@ -1,6 +1,7 @@
 package com.bdg.bdgebay.controller.user;
 
 import com.bdg.bdgebay.common.ApiResponse;
+import com.bdg.bdgebay.common.UserTransferObject;
 import com.bdg.bdgebay.dto.user.UserCreationRequest;
 import com.bdg.bdgebay.dto.user.UserUpdateRequest;
 import com.bdg.bdgebay.entity.User;
@@ -26,10 +27,10 @@ public class UserController {
 
 
     @RequestMapping(path = "/user/create", method = RequestMethod.POST)
-    public ApiResponse<User> createUser(@RequestBody UserCreationRequest creationRequest) {
+    public ApiResponse<UserTransferObject> createUser(@RequestBody UserCreationRequest creationRequest) {
 
         try {
-            return new ApiResponse<>(userService.create(creationRequest), "Success", HttpStatus.CREATED);
+            return new ApiResponse<>(modelMapper.map(userService.create(creationRequest), UserTransferObject.class), "Success", HttpStatus.CREATED);
         } catch (UserNameExistException ex) {
             return new ApiResponse<>(null, ex.getMessage(), HttpStatus.BAD_REQUEST);
         }
